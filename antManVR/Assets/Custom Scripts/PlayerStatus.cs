9cbb1f3ada5eed7ddb5f5250570b4b8d77839692
Courior <bamfer6@gmail.com>
2016-09-10 19:30:26 +0100

    new proj

---
+++ antManVR/Assets/Custom Scripts/PlayerStatus.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class PlayerStatus : MonoBehaviour {
+	private bool _shrunk;
+	public bool shrunk {
+		get { return _shrunk; }
+		set{
+			if (_shrunk == value)
+				return;
+			else
+				_shrunk = value;
+				this.ChangeSize(value);
+		}
+
+	}
+	private void ChangeSize(bool shrink){
+		GameObject antMan = GameObject.Find("PlayerObject_[CameraRig]");
+		if (antMan != null) {
+			if (!shrink) {
+				antMan.transform.localScale = new Vector3(1F, 1F, 1F);	
+			} else {
+				antMan.transform.localScale = new Vector3(0.1F, 0.1F, 0.1F);	
+			}
+		}
+		else{
+			Debug.LogError ("antMan not found");
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		_shrunk = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ antManVR/Assets/Custom Scripts/Shrink.cs
@@ -0,0 +1,47 @@
+﻿namespace ANTMAN
+{
+
+
+	using UnityEngine;
+	using System.Collections;
+	using VRTK;
+
+
+	public class Shrink : MonoBehaviour {
+		private PlayerStatus status;
+		private void Start()
+		{
+			if (GetComponent<VRTK_ControllerEvents>() == null)
+			{
+				Debug.LogError("VRTK_ControllerEvents_ListenerExample is required to be attached to a SteamVR Controller that has the VRTK_ControllerEvents script attached to it");
+				return;
+			}
+			if (this.GetComponentInParent<PlayerStatus>()== null)
+			{
+				Debug.LogError("PlayerStatus is required to be attached to the CaneraRig");
+				return;
+			}
+
+
+			status = this.GetComponentInParent<PlayerStatus>();
+
+
+			//Setup controller event listeners
+
+			//GetComponent<VRTK_ControllerEvents>().TouchpadPressed += new ControllerInteractionEventHandler(DoTouchpadPressed);
+			GetComponent<VRTK_ControllerEvents>().TouchpadReleased += new ControllerInteractionEventHandler(DoShrink);
+
+		}
+
+		private void DoShrink(object sender, ControllerInteractionEventArgs e){
+				status.shrunk = true;
+		
+		}
+		private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
+		{
+			Debug.Log("Controller on index '" + index + "' " + button + " has been " + action
+				+ " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
+		}
+
+	}
+}
